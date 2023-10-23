8294fa23afede954cd24732585de073b4f68cdc0
Andon Mefford <andonmefford@outlook.com>
2023-10-22 23:45:10 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
 function augmentingPath(graph, start, end) {
+    const queue = [start];
+    const visited = {};
+    visited[start] = true;
+    const parent = {};
+      while (queue.length > 0) {
+      const currentNode = queue.shift();
+      if (currentNode === end) {
+        const path = [];
+        let current = end;
+        while (current) {
+          path.unshift(current);
+          current = parent[current];
+        }
+          return path;
+      }
+        const neighbors = graph[currentNode];
+      if (neighbors) {
+        for (const neighbor of Object.keys(neighbors)) {
+          if (!visited[neighbor]) {
+            visited[neighbor] = true;
+            parent[neighbor] = currentNode;
+            queue.push(neighbor);
+          }
+        }
+      }
+    }
     return [];
 }
